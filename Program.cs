3de1dafa0352be9c95f85aaf8fd373dfd8e81e6c
Sean McLellan <sean@baristalabs.io>
2017-01-20 13:42:16 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ChakraSharp_Sample
+{
+    using ChakraSharp;
+    using System;
+    using System.Diagnostics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr runtimePtr;
+            var result = ChakraCommon.JsCreateRuntime(_JsRuntimeAttributes.JsRuntimeAttributeNone, null, out runtimePtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            IntPtr contextPtr;
+            result = ChakraCommon.JsCreateContext(runtimePtr, out contextPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            result = ChakraCommon.JsSetCurrentContext(contextPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            var sourceUrl = "[eval code]";
+            IntPtr sourceUrlPtr;
+            result = ChakraCore.JsCreateString(sourceUrl, (ulong)sourceUrl.Length, out sourceUrlPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            var script = "6*7";
+            IntPtr scriptPtr;
+            result = ChakraCore.JsCreateString(script, (ulong)script.Length, out scriptPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            IntPtr resultPtr;
+            result = ChakraCore.JsRun(scriptPtr, 0, sourceUrlPtr, _JsParseScriptAttributes.JsParseScriptAttributeNone, out resultPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            IntPtr stringResultPtr;
+            result = ChakraCommon.JsConvertValueToString(resultPtr, out stringResultPtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            string strResult;
+            unsafe
+            {
+                ulong written = 0;
+                byte[] buffer = new byte[100];
+                fixed(byte* bufferPtr = buffer)
+                {
+                    result = ChakraCore.JsCopyString(stringResultPtr, bufferPtr, (ulong)buffer.LongLength, ref written);
+                    Debug.Assert(result == _JsErrorCode.JsNoError);
+                }
+                strResult = System.Text.Encoding.UTF8.GetString(buffer, 0 , (int)written);
+            }
+
+            result = ChakraCommon.JsSetCurrentContext(IntPtr.Zero);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            result = ChakraCommon.JsDisposeRuntime(runtimePtr);
+            Debug.Assert(result == _JsErrorCode.JsNoError);
+
+            Console.WriteLine(string.Format("The result of {0} is {1}", script, strResult));
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
