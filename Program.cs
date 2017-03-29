564b2526e7f823b5b07629f18ee58ef673845afe
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-03-29 10:05:07 -0400

    Update samples to match latest ChakraCore and generated interface

--- Program.cs
+++ Program.cs
@@ -15,39 +15,39 @@
 
             //Create a new ChakraCore JS runtime.
             IntPtr runtimePtr;
-            var result = ChakraCommon.JsCreateRuntime(_JsRuntimeAttributes.JsRuntimeAttributeNone, null, out runtimePtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            var result = ChakraCommon.JsCreateRuntime(JsRuntimeAttributes.JsRuntimeAttributeNone, null, out runtimePtr);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Create a context
             IntPtr contextPtr;
             result = ChakraCommon.JsCreateContext(runtimePtr, out contextPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Set the current context on the runtime -- multiple contexts can be created, but only one can be active on a runtime at a time.
             result = ChakraCommon.JsSetCurrentContext(contextPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Create a string that contains a small description of our code. This string shows up when exceptions occur, in a full application this probably should indicate the physical location of the script.
             var sourceUrl = "[eval code]";
             IntPtr sourceUrlPtr;
             result = ChakraCore.JsCreateString(sourceUrl, (ulong)sourceUrl.Length, out sourceUrlPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Create the actual script that we're going to execute.
             var script = "6*7";
             IntPtr scriptPtr;
             result = ChakraCore.JsCreateString(script, (ulong)script.Length, out scriptPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Tell the JavaScript engine to execute our script, associating the sourceUrl we created with it.
             IntPtr resultPtr;
-            result = ChakraCore.JsRun(scriptPtr, 0, sourceUrlPtr, _JsParseScriptAttributes.JsParseScriptAttributeNone, out resultPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            result = ChakraCore.JsRun(scriptPtr, 0, sourceUrlPtr, JsParseScriptAttributes.JsParseScriptAttributeNone, out resultPtr);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Convert the result (which is a number) to a string.
             IntPtr stringResultPtr;
             result = ChakraCommon.JsConvertValueToString(resultPtr, out stringResultPtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Obtain and copy the string from the runtime to a local variable.
             string strResult;
@@ -60,7 +60,7 @@
                 fixed (sbyte* bufferPtr = buffer)
                 {
                     result = ChakraCore.JsCopyString(stringResultPtr, bufferPtr, 0, out written);
-                    Debug.Assert(result == _JsErrorCode.JsNoError);
+                    Debug.Assert(result == JsErrorCode.JsNoError);
                     targetLength = written;
                 }
 
@@ -70,7 +70,7 @@
                 fixed (sbyte* bufferPtr = buffer)
                 {
                     result = ChakraCore.JsCopyString(stringResultPtr, bufferPtr, (ulong)buffer.LongLength, out written);
-                    Debug.Assert(result == _JsErrorCode.JsNoError);
+                    Debug.Assert(result == JsErrorCode.JsNoError);
 
                     //Create a new string from the sbyte buffer.
                     strResult = new string(bufferPtr, 0, (int)written);
@@ -79,11 +79,11 @@
 
             //As we're all done, set the current context to nothing.
             result = ChakraCommon.JsSetCurrentContext(IntPtr.Zero);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Dispose of the runtime.
             result = ChakraCommon.JsDisposeRuntime(runtimePtr);
-            Debug.Assert(result == _JsErrorCode.JsNoError);
+            Debug.Assert(result == JsErrorCode.JsNoError);
 
             //Output what we got.
             Console.WriteLine(string.Format("The result of {0} is {1}", script, strResult));
